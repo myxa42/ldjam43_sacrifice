3a08e4545879a112d09e92ef14a09a7c15d1c587
Evgeniya Zapolnova <mini.ezhik@gmail.com>
2018-12-01 19:12:15 +0200

    Started to implement buildings

---
+++ Sacrifice/Assets/Scripts/Game/Building.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Resource
+    {
+        public BlockType blockType;
+        public int count;
+    }
+
+    public Image activeImage;
+    public Image inactiveImage;
+    public Resource[] requiredResources;
+    private Dictionary<BlockType, int> availableResources = new Dictionary<BlockType, int>();
+    public GameController gameController;
+    public BlockType product=BlockType.Empty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeImage.enabled = false;
+        inactiveImage.enabled = true;
+    }
+
+    public void AddResource(BlockType type,int count=1)
+    {
+        availableResources[type] += count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool haveEnoughResources = true;
+        foreach(var res in requiredResources)
+        {
+            int count;
+            if(!availableResources.TryGetValue(res.blockType,out count)||count < res.count)
+            {
+                haveEnoughResources = false;
+                break;
+            }
+        }
+
+        if (!haveEnoughResources)
+        {
+            return;
+        }
+
+        foreach (var res in requiredResources)
+        {
+            availableResources[res.blockType] -= res.count;
+        }
+
+        if (product != BlockType.Empty)
+        {
+            gameController.pendingBlocks.Enqueue(product);
+        }
+    }
+}
